3aabe82ce81e7aa0d5e580f3a4744a08c0723736
Wali Ullah <waliullahbiplob786@gmail.com>
2026-07-19 09:21:27 +0600

    added

---
+++ storage/framework/views/ef67979faf7a615ae2f43eeaf615d706.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('content'); ?>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.frontend.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\laragon\www\erp\resources\views/frontend/home.blade.php ENDPATH**/ ?>
